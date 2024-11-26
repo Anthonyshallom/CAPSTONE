b1366ecaf0839df6c7811672b35e21f9d56784d0
Anthonyshallom <anthonyshallom7@gmail.com>
2024-11-26 05:47:32 -0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path = require('path');
+const port = 5000;
+const _db  =  require ('./_dbConnect');
+const Router = require ("./route");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+
+
+
+const app = express();
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('api',Router )
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
+
+dotenv.config();
+_db().then(
+
+    app.listen(port,()=>{
+        console.log(`server is running on port ${port}`)
+    })
+)
+
+
+// 
+ 
+
+
+// const express = require('express')
+
+
+// const path = require('path')
+
+// const port = 5000
+// const app = express();
+// app.use(express.json())
+
+// app.use(express.static(path.join(__dirname,'public')))
+// _db().then()
+
+
+
+// app.listen(port,() =>{
+//     console.log(`server is runing in port ${port}`)
+// })
